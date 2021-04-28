f1d18a0fd9580c28557fea6ffb2f3d686a0200cf
jarrodwatts <35651410+jarrodwatts@users.noreply.github.com>
2021-04-28 23:17:08 +1000

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import React from "react";
 
-export default function Home() {
+/**
+ * Home: The Entry point to the application
+ * @return {JSX.Element} The JSX Code for the Home Page
+ */
+export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +20,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +61,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
